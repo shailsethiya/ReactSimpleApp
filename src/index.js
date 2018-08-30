27c251e400b102f3477a9a9ffd4a4e5be1b78483
Srinivas Kalyani <Srinivaskalyani12@gmail.com>
2018-08-30 21:01:25 +0530

    feat: react simple application

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React,{Components} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyARHm2zZJC0A3acdMSXGC4AuXFGzwk_5ik';
+
+class App extends Components{
+	constructor(props){
+		super(props);
+		this.state = { 
+			videos : [],
+            selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	    }
+   
+
+         videoSearch(term){
+            YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+                selectedVideo: videos[0]
+			 })
+		});
+        }
+
+  render() {
+	 return(
+	 	<div> 
+	 	   <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+	 	   <VideoDetail video={this.state.selectedVideo} />
+	 	   <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos ={this.state.videos}/>
+	 	</div>
+		);
+	}
+}
+
+ReactDOM.render(<App /> , document.querySelector('.container'));
